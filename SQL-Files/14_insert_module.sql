df9c393df8daccb4c63ba7a84e4b30e590c5681f
Tummala-Dinesh <dineshtummala111@gmail.com>
2026-05-18 20:43:43 +0530

    Add files via upload

---
+++ SQL-Files/14_insert_module.sql
@@ -0,0 +1,11 @@
+INSERT INTO Module (ModuleID, CourseID, Title, OrderIndex) VALUES
+(801, 501, 'ER Diagrams', 1),
+(802, 501, 'SQL Joins', 2),
+(803, 502, 'Kernels', 1),
+(804, 503, 'Logic', 1),
+(805, 505, 'Lists/Tuples', 1),
+(806, 506, 'Classes', 1),
+(807, 507, 'HTML5', 1),
+(808, 508, 'Sorting', 1),
+(809, 509, 'CPU Design', 1),
+(810, 510, 'Patterns', 1);
